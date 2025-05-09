0f1f02eb7912d8528da221e3b9a067a3aa1fe046
Madiyan1960 <103851348+Madiyan1960@users.noreply.github.com>
2025-05-09 09:19:35 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,24 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('my-pwa-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/css/style.css',
+        '/js/script.js',
+        '/manifest.json',
+        '/icons/icon-192x192.png',
+        '/icons/icon-512x512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
 
