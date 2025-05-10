ec085d53e64147c7d2130f1adb99a760f5185e3f
Madiyan1960 <103851348+Madiyan1960@users.noreply.github.com>
2025-05-10 11:01:34 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,8 +7,8 @@ self.addEventListener('install', (event) => {
         '/css/style.css',
         '/js/script.js',
         '/manifest.json',
-        '/icons/icon-192x192.png',
-        '/icons/icon-512x512.png'
+        '/icons/icon-192.png',
+        '/icons/icon-512.png'
       ]);
     })
   );
